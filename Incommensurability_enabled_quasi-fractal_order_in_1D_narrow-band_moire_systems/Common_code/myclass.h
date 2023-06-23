e9e442c381b809e40380d8b8f1d2ef652e0dd320
gmiguel17 <miguel.m.goncalves@ist.utl.pt>
2023-06-23 23:52:57 +0100

    Twisted boundaries compiled and ran smoothly

--- Incommensurability_enabled_quasi-fractal_order_in_1D_narrow-band_moire_systems/Common_code/myclass.h
+++ Incommensurability_enabled_quasi-fractal_order_in_1D_narrow-band_moire_systems/Common_code/myclass.h
@@ -1,49 +1,39 @@
-//The following ifndef/define/endif pattern is called a 
-//scope guard, and prevents the C++ compiler (actually, preprocessor)
-//from including a header file more than once.
+// The following ifndef/define/endif pattern is called a
+// scope guard, and prevents the C++ compiler (actually, preprocessor)
+// from including a header file more than once.
 #ifndef __MY_CLASS_H_
 #define __MY_CLASS_H_
 
-#include <string>
 #include <iostream>
+#include <string>
 
-class MyClass
-    {
-    int i_;
-    std::string name_;
-    public:
-    
-    //Default constructor
-    MyClass() : i_(0) { }
+class MyClass {
+  int i_;
+  std::string name_;
 
-    MyClass(std::string name, int i)
-      : i_(i),
-        name_(name)
-        { }
+ public:
+  // Default constructor
+  MyClass() : i_(0) {}
 
-    std::string const&
-    name() const { return name_; }
+  MyClass(std::string name, int i) : i_(i), name_(name) {}
 
-    int
-    value() const { return i_; }
+  std::string const& name() const { return name_; }
 
-    };
+  int value() const { return i_; }
+};
 
 //
-//Defining this method enables printing of MyClass objects
-//using cout << m << endl; where m is a MyClass instance.
+// Defining this method enables printing of MyClass objects
+// using cout << m << endl; where m is a MyClass instance.
 //
-//It also allows printing using the print,println,printf, and
-//printfln functions defined by ITensor. 
-//Use the "%s" token to print custom objects such as a MyClass
-//object with printf and printfln.
+// It also allows printing using the print,println,printf, and
+// printfln functions defined by ITensor.
+// Use the "%s" token to print custom objects such as a MyClass
+// object with printf and printfln.
 //
-inline std::ostream&
-operator<<(std::ostream& s, MyClass const& m)
-    {
-    s << "MyClass(" << m.name() << "," << m.value() << ")";
-    return s;
-    }
+inline std::ostream& operator<<(std::ostream& s, MyClass const& m) {
+  s << "MyClass(" << m.name() << "," << m.value() << ")";
+  return s;
+}
 
-
-#endif //__MY_CLASS_H
+#endif  //__MY_CLASS_H
